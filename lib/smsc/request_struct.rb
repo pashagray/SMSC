5d86718bcfe86f9991d434f84c7ad3136e0b77bc
Pavel Tkachenko <tpepost@gmail.com>
2018-02-11 19:03:22 +0600

    [Fix] Every #call should return Success or Failure monad

--- lib/smsc/request_struct.rb
+++ lib/smsc/request_struct.rb
@@ -4,9 +4,11 @@ module SMSC
   extend Dry::Monads::Try::Mixin
 
   class RequestStruct < Dry::Struct
+    include Dry::Monads::Result::Mixin
+
     NETWORK_ERRORS = [Errno::ECONNREFUSED].freeze
     ERRORS = {
-      2 => :authorize
+      2 => :authorize_error
     }
     API_PATH = "https://smsc.kz/sys/".freeze
 
@@ -21,12 +23,12 @@ module SMSC
       res = SMSC::Try(*NETWORK_ERRORS) do
         Net::HTTP.post_form(uri, __attributes__)
       end
-      return { error: :network } if res.error?
+      return Failure(:network_error) if res.error?
       json = JSON.parse(res.value!.body, symbolize_names: true)
       if json[:error_code]
-        return { error: ERRORS[json[:error_code]] } 
+        return Failure(ERRORS[json[:error_code]])
       else
-        json
+        Success(json)
       end
     end
   end
--- spec/smsc/balance_spec.rb
+++ spec/smsc/balance_spec.rb
@@ -3,14 +3,14 @@ RSpec.describe SMSC::Balance do
     it "returns error on CONNECTION_REFUSED" do
       stub_request(:post, "https://smsc.kz/sys//balance.php").to_raise(Errno::ECONNREFUSED)
       request = SMSC::Balance.new(login: "login", psw: "password")
-      expect(request.call).to eq({ error: :network })
+      expect(request.call.value).to eq(:network_error)
     end
 
     context "wrong credentials" do
       it "returns error" do
         stub_request(:post, "https://smsc.kz/sys//balance.php").to_return(body: File.new('spec/smsc/fixtures/wrong_credentials.json'), status: 200)
         request = SMSC::Balance.new(login: "login", psw: "password")
-        expect(request.call).to eq({ error: :authorize })
+        expect(request.call.value).to eq(:authorize_error)
       end
     end
 
@@ -18,7 +18,7 @@ RSpec.describe SMSC::Balance do
       it "returns data on success request" do
         stub_request(:post, "https://smsc.kz/sys//balance.php").to_return(body: File.new('spec/smsc/fixtures/balance.json'), status: 200)
         request = SMSC::Balance.new(login: "login", psw: "password")
-        expect(request.call).to eq({ balance: "0.00", currency: "KZT" })
+        expect(request.call.value).to eq({ balance: "0.00", currency: "KZT" })
       end
     end
   end
